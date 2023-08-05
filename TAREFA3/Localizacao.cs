0993c4e51c78eeee10ca86ac5790dfb240611e90
Wendi Ramos <wendiramos12@gmail.com>
2023-08-05 20:37:58 -0300

    feat: tarefa 3

---
+++ TAREFA3/Localizacao.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TAREFA3
+{
+    class Localizacao
+    {
+        [JsonPropertyName("cep")]
+        public string CEP { get; set; }
+
+        [JsonPropertyName("logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonPropertyName("complemento")]
+        public string Complemento { get; set; }
+
+        [JsonPropertyName("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonPropertyName("localidade")]
+        public string Localidade { get; set; }
+
+        [JsonPropertyName("uf")]
+        public string UF { get; set; }
+
+        [JsonPropertyName("ibge")]
+        public string IBGE { get; set; }
+
+        [JsonPropertyName("gia")]
+        public string GIA { get; set; }
+
+        public string? Unidade { get; set; }
+    }
+}
---
+++ TAREFA3/Program.cs
@@ -0,0 +1,59 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TAREFA3
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using var reader = new StreamReader("../../../../CEPs.csv");
+            using var writer = new StreamWriter("../../../../CEPs-tarefa3.csv");
+
+            var csvConfig = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                BadDataFound = null,
+            };
+
+            using var csvOriginal = new CsvReader(reader, csvConfig);
+            var localizacoes = csvOriginal.GetRecords<Localizacao>().ToList();
+
+            List<Localizacao> localizacoesAtualizadas = new List<Localizacao>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                foreach (var localizacao in localizacoes)
+                {
+                    var cep = Regex.Replace(localizacao.CEP, "[^0-9]", "");
+                    try
+                    {
+                        string url = $"https://viacep.com.br/ws/{cep}/json/";
+                        HttpResponseMessage response = await client.GetAsync(url);
+                        
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string responseBody = await response.Content.ReadAsStringAsync();
+                            Localizacao cepInfo = JsonSerializer.Deserialize<Localizacao>(responseBody);
+                            localizacoesAtualizadas.Add(cepInfo);
+                        }
+                    }
+                    catch (HttpRequestException)
+                    {
+
+                    }
+                }
+            }
+
+            using var csvAlterado = new CsvWriter(writer, csvConfig);
+            csvAlterado.WriteRecords(localizacoesAtualizadas);
+        }
+    }
+}
