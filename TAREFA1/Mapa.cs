c95f6f3519e4c4cc81dcf24185236335c5d8031e
Wendi Ramos <wendiramos12@gmail.com>
2023-08-05 18:04:05 -0300

    feat: tarefa 1

---
+++ TAREFA1/Mapa.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+
+namespace TAREFA1
+{
+    class Mapa
+    {
+        [Index(0)]
+        public string Local { get; set; }
+
+        [Index(1)]
+        public string Populacao { get; set; }
+    }
+}
--- TAREFA1/Program.cs
+++ TAREFA1/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using CsvHelper;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace TAREFA1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using var reader = new StreamReader("../../../../mapa.csv");
+            using var writer = new StreamWriter("../../../../mapa-tarefa1.csv");
+
+            var csvConfig = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                BadDataFound = null,
+            };
+
+            using var csvOriginal = new CsvReader(reader, csvConfig);
+            var mapas = csvOriginal.GetRecords<Mapa>().ToList();
+
+            var mapasAtualizados = mapas.Select(mapa => new Mapa()
+            {
+                Local = mapa.Local,
+                Populacao = int.Parse(mapa.Populacao).ToString("N0")
+            });
+
+            using var csvAlterado = new CsvWriter(writer, csvConfig);
+            csvAlterado.WriteRecords(mapasAtualizados);
         }
     }
 }
