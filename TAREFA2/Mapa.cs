1d46fdead8a72c13488ef5ed6661a739ecb55b77
Wendi Ramos <wendiramos12@gmail.com>
2023-08-05 19:28:43 -0300

    feat: tarefa 2

---
+++ TAREFA2/Mapa.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+
+namespace TAREFA2
+{
+    class Mapa
+    {
+        [Index(0)]
+        public string Local { get; set; }
+
+        [Index(1)]
+        public int Populacao { get; set; }
+    }
+}
--- TAREFA2/Program.cs
+++ TAREFA2/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using CsvHelper;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace TAREFA2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using var reader = new StreamReader("../../../../mapa.csv");
+            using var writer = new StreamWriter("../../../../mapa-tarefa2.csv");
+
+            var csvConfig = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                BadDataFound = null,
+            };
+
+            using var csvOriginal = new CsvReader(reader, csvConfig);
+            var mapas = csvOriginal.GetRecords<Mapa>().ToList();
+
+            int tamanho = mapas.Count;
+            for (int i = 0; i < tamanho - 1; i++)
+            {
+                for (int j = 0; j < tamanho - i - 1; j++)
+                {
+                    if (mapas[j].Populacao < mapas[j + 1].Populacao)
+                    {
+                        Mapa temp = mapas[j];
+                        mapas[j] = mapas[j + 1];
+                        mapas[j + 1] = temp;
+                    }
+                }
+            }
+
+            using var csvAlterado = new CsvWriter(writer, csvConfig);
+            csvAlterado.WriteRecords(mapas);
         }
     }
 }
